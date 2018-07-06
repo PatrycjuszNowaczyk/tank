f51b225ec4b8bdacc009cad828a12436ebfeb3ac
Marlena Adamczak <marlena.adamczak@gmail.com>
2018-07-06 14:44:57 +0200

    poprawki w nazwach zmiennych

--- board.php
+++ board.php
@@ -1,81 +1,66 @@
 <?php
-$ch = curl_init('http://tank.iai.ninja/api/get-current-board.php');
-curl_setopt($ch, CURLOPT_URL, "http://tank.iai.ninja/api/get-current-board.php");
-curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
-curl_setopt($ch, CURLOPT_CUSTOMREQUEST, "GET");
-$jsonResponse = curl_exec($ch);
-$response = json_decode($jsonResponse, true);
+	$ch = curl_init('http://tank.iai.ninja/api/get-current-board.php');
+	curl_setopt($ch, CURLOPT_URL, "http://tank.iai.ninja/api/get-current-board.php");
+	curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
+	curl_setopt($ch, CURLOPT_CUSTOMREQUEST, "GET");
+	$jsonResponse = curl_exec($ch);
+	$response = json_decode($jsonResponse, true);
 
-$gameId = $response['id'];
-$gameName = $response['name'];
-$gameBoardSize = $response['settings']['boardSize'];
-$size=$gameBoardSize;
-$gameBoard = $response['board'];
-$gameBoardNew = array();
-$gamers = array();
+	$gameId = $response['id'];
+	$gameName = $response['name'];
+	$gameBoardSize = $response['settings']['boardSize'];
+	$size=$gameBoardSize;
+	$gameBoard = $response['board'];
+	$gameBoardNew = array();
+	$gamers = array();
 
-foreach ($gameBoard as $val) {
-  $gameBoardNew[$val['position']] = $val['type'];
-  $gamers = $val['name'];
-  $gamers = array_unique($gamers);
-}
+	foreach ($gameBoard as $val) {
+		$gameBoardNew[$val['position']] = $val['type'];
+		$gamers = $val['name'];
+	}
 
-echo '<pre>';
-print_r(array_unique($gameBoardNew));
-echo '</pre>';
-
-$tablica = array();
-$color = 'white';
-
-
+	$gamers = array_unique($gamers);
+	
+	$tablica = array();
 
     $table = '';
-    $id = '';
-    $class = '';
 
     $table .= '<table style="border-collapse: collapse">';
-    foreach (range(1, $size) as $row) {
+    foreach (range(1, $gameBoardSize) as $row) {
         $table .= "<tr>";
 
 		$letters = array();
-		$lastLetter = chr($size-1+ord('A'));
+		$lastLetter = chr($gameBoardSize-1+ord('A'));
 
 		if (ord($lastLetter) > ord('Z')) {
 			$letters = range('A', 'Z');
 			foreach(range('A', 'Z') as $letter1) {
 				foreach(range('A', 'Z') as $letter2) {
 					$letters[] = $letter1 . $letter2;
-					$letters = array_slice($letters, 0, $size);
+					$letters = array_slice($letters, 0, $gameBoardSize);
 				}
 			}
 		}
 		else {
-			$letters = range('A', chr($size-1+ord('A')));
+			$letters = range('A', chr($gameBoardSize-1+ord('A')));
 		}
 
         foreach ($letters as $col) {
             $tablica[$col][$row] = $col . $row;
             $id = $col . $row;
             $style = $gameBoardNew[$id];
-//$style
-// var_Dump($id );
+
             $table .= '<td id="'. $id . '_'.'" class="' . $class . '" style="width: 25px; height: 25px;  border-collapse: collapse; border: 1px solid black; vertical-align: ';
             $table .= 'top">' . $tablica[$col][$row] . '</td>';
         }
         $table .= "</tr>";
     }
+	
     $table .= "</table>";
 
     $table .= str_repeat("<br>", 1);
 
-
-
-
     echo $table;
 
-
-
-
-//createBoard(40);
 
 ?>
